c56edc0d8569e7fc9e35344b09b91bc08bdb6486
MEENAKSHI KOKKILIGADDA <31063803+Meenakshi121@users.noreply.github.com>
2022-09-08 16:56:35 +0530

    Add files via upload

---
+++ mobile.py
@@ -0,0 +1,9 @@
+def vivo():
+    print("Features:")
+    print("Battery:5000mhg\n RAM:6GB\N Cost:12000rs")
+          
+def realme():
+    print("Features:")
+    print("Battery:5500mhg\n RAM:32GB\N Cost:10000rs")
+          
+       
